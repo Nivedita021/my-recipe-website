67768ec2025f7b3ed97cd3c03c15bc37b5dd3b21
Nivedita Chatterjee <99239032+Nivedita021@users.noreply.github.com>
2023-10-10 11:48:26 +0530

    Update Veg.jsx for mobile users

--- src/components/Veg.jsx
+++ src/components/Veg.jsx
@@ -1,6 +1,7 @@
 import React from 'react';            
 import { useState } from 'react';
 import { useEffect } from 'react';
+import {motion} from 'framer-motion';
 import styled from "styled-components";
 import {Splide, SplideSlide} from '@splidejs/react-splide';
 import '@splidejs/splide/dist/css/splide.min.css';
@@ -27,85 +28,51 @@ getVeg();
      
   };
     return (
-      <div>
-       
-       
-      <Wrapper>
+     <>
           <Typography variant='h4' sx={{m: 3}}>
            Our Vegetarian Picks
           </Typography>
-          <Splide options={{
-              perPage:3,
-              arrows:false,
-              pagination:false,
-              drag:'free',
-              gap:"5rem",
-          }}>
-          {veg.map((recipe)=>{
-              return(
-                  <SplideSlide>
-      <Card>
-        <Link to={"/recipe/" + recipe.id}>
-          <Typography>
-              {recipe.title}
-          </Typography>
-          <img src={recipe.image} alt={recipe.title} />
-          <Gradient />
-          </Link>
-      </Card>
-      </SplideSlide>
-              );
-          })}
-          </Splide>
-      </Wrapper>
-              
-            
-              </div>
+          <Grid
+        animate={{opacity: 1}}
+        initial={{opacity: 0}}
+        exit={{opacity: 0}}
+        transition={{duration: 0.5}}
+        >
+       {
+        veg.map((item)=>{
+            return(
+                <Card key={item.id}>
+                    <Link to={"/recipe/" + item.id}>
+                    <img src={item.image} alt="" />
+                    <Typography variant='h6'>{item.title}</Typography>
+                    </Link>
+                </Card>
+            )
+        })
+       }
+        </Grid>
+
+              </>
     );
 }
-const Wrapper= styled.div`
-margin: 4rem 0rem;
-`;
-const Card= styled.div`
-min-height:20rem;
-border-radius:2rem;
-overflow:hidden;
-position:relative;
+const Card =styled.div`
 img{
-border-radius:2rem;
-position:absolute;
-left:0;
-width:100%;
-height:100%;
-object-fit:cover;
-
-}
-p{
-    position: absolute;
-    z-index:10;
-    left:50%;
-    bottom:0%;
-    transform:translate(-50%, 0%);
-    color:white;
     width:100%;
-    text-align:center;
-    font-weight:600;
-    font-size:1rem;
-    height:40%;
-    display:flex;
-    justify-content:center;
-    align-items:center;
+    height:60%;
+    border-radius:2rem;
+   
 }
-
+a{
+  text-decoration: none;  
+}
+h6{
+    text-align: center;
+    padding: 1rem;
+}
 `;
-const Gradient=styled.div`
-z-index:3;
-position:absolute;
-width:100%;
-height:100%;
-background:linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.5));
-
-
-
+const Grid=styled(motion.div)`
+display:grid;
+grid-template-columns: repeat(auto-fit, minmax(15rem,1fr));
+grid-gap:1rem;
 `;
 export default Veg;
